49fe2b6a1e4f667c1c544596a1f2428c37d0278b
gja5899 <anh-duc.thai@teliacompany.com>
2023-01-22 19:55:10 +0200

    basic components and asset for the front end

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { FormField,Loader } from '../components'
+const CreatePost = () => {
+  return (
+    <div>
+      Create post
+    </div>
+  )
+}
+
+export default CreatePost
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Loader, Card, FormField } from "../components";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return <h2 className="mt-5 font-bold text-[#6449ff] text-xl">{title}</h2>;
+};
+const Home = () => {
+  const [Loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The community showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          I love you, always, but life has to move on.
+        </p>
+      </div>
+      <div className="mt-16">
+        <FormField />
+      </div>
+      <div className="mt-10">
+        {Loading ? (
+          <div className="flex justify-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing result for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No search result" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
